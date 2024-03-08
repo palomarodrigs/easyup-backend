91393a215db2a2297be529bcdb61a0a061bc538c
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-03-08 11:07:44 -0300

    feat: add image table and add setup prisma client

---
+++ prisma/migrations/20240308135750_add_image_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "transcription" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
