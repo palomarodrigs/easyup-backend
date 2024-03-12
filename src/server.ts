4d198cff8c09eadba97a9189dfb4426432b26261
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-03-12 19:07:40 -0300

    feat: add cors setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { uploadImageRoute } from './routes/upload-image'
 import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(uploadImageRoute)
 app.register(createTranscriptionRoute)
